5561b5f9a6a363bdcc7074a0a439b3e8ed4d10eb
Emre Cebeci <emreyigitcbc@gmail.com>
2022-08-20 14:48:43 +0300

    quickfix
    fix for downloading non-episode 0 mangas

--- websites/mangagecesi.js
+++ websites/mangagecesi.js
@@ -4,8 +4,8 @@ const { download, sleep } = require("../functions")
 module.exports = {
     name: "Manga Gecesi",
     author: "Emre Cebeci",
-    version: "1.0",
-    url_message: "Please enter the episode 0 page URL of series: ",
+    version: "1.1",
+    url_message: "Please enter the first episode page URL of series: ",
     light_novel: false,
     has_thumbnail: false,
     browser: false,
@@ -19,16 +19,17 @@ module.exports = {
         let comic_name = await page.url().replace(/[^]*cizgi-romanlar[/]/g, "").split("/")[0]
         let response = await axios.get(`https://mangagecesi.com/v1/comics/${comic_name}/chapter-list`)
         let chapters = response.data.length
+        let first_chapter = Number(response.data[0].name) // It might be 0 or 1
         // Start fetching images
         let total = 0;
-        for (var i = 0; i < chapters; i++) {
+        for (var i = first_chapter; i < chapters; i++) {
             let chapter_data = {}
             try {
                 chapter_data = await axios.get(`https://mangagecesi.com/v1/comics/${comic_name}/chapters/bolum-${i}`)
                 console.log(i + ". bölüm indiriliyor (" + chapter_data.data.chapter.pages.length + " sayfa)")
                 for (var [index, url] of chapter_data.data.chapter.pages.entries()) {
-                    await download(url, total + ".jpg")
-                    console.log(i + ". bölüm sayfa " + index + " indirildi.")
+                    await download(url, total + ".gif")
+                    console.log(i + ". bölüm sayfa " + (index+1) + " indirildi.")
                     global.results.pages.push(total + ".jpg")
                     total += 1
                 }
